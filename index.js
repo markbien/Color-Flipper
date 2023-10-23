ad87586808f50d8c44c1306de9f8cff1ce36b8c1
Mark Bien <48498353+markbien@users.noreply.github.com>
2023-10-23 15:20:53 +0800

    Add function to randomize hex color

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const main = document.querySelector('main');
 const changeColorButton = document.querySelector('.container button');
 const currentColorDisplayed = document.querySelector('.color');
+const colorOptions = document.querySelector('.nav-links');
+const simpleOption = colorOptions.querySelector('li:nth-child(1)');
+const hexOption = colorOptions.querySelector('li:nth-child(2)');
+let isHexSelected = false;
 
 function generateRandomNumber(){
   let num = Math.floor(Math.random() * 256);
@@ -15,10 +19,35 @@ function changeCurrentColorDisplay(newColor){
   currentColorDisplayed.textContent = newColor;
 }
 
-function randomizeBackgroundColor(){
-  const randomColor = generateRandomRGBColor();
+function randomizeBackgroundColor(randomize){
+  // randomize is a function that will be passed
+  const randomColor = randomize();
   changeCurrentColorDisplay(randomColor);
   main.style.backgroundColor = randomColor;
 }
 
-changeColorButton.addEventListener('click', randomizeBackgroundColor);
+function rgbToHex(r, g, b) {
+  return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+}
+
+function generateRandomHexColor(){
+  return rgbToHex(generateRandomNumber(),generateRandomNumber(),generateRandomNumber());
+}
+
+changeColorButton.addEventListener('click', function(){
+  if (!isHexSelected) {
+    randomizeBackgroundColor(generateRandomRGBColor);
+  } else {
+    randomizeBackgroundColor(generateRandomHexColor);
+  }
+});
+
+simpleOption.addEventListener('click', function(){
+  changeCurrentColorDisplay('rgb(?,?,?)');
+  isHexSelected = false;
+});
+
+hexOption.addEventListener('click', function(){
+  changeCurrentColorDisplay('#??????');
+  isHexSelected = true;
+});
